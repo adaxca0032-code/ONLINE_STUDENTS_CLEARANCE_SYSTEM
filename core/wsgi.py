a229f8cc372295cf4f267d95bd2e2730e3fed70f
m.nzutu <adaxca0032@gmail.com>
2026-06-21 16:46:51 +0300

    nimeongeza automatic migration ndani ya wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -1,16 +1,15 @@
-"""
-WSGI config for core project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Kazi ya kulazimisha migration mtandaoni kiotomatiki
+try:
+    from django.core.management import execute_from_command_line
+    print("Inalazimisha migrations kufanya kazi mtandaoni...")
+    execute_from_command_line([sys.argv[0], 'migrate', '--noinput'])
+except Exception as e:
+    print(f"Migration imefeli lakini tunawasha mtambo: {e}")
 
 application = get_wsgi_application()
